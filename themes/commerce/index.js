928515b9dc6aba2be70c86c4b0ee7d8a89a3a492
BuildTools <unconfigured@null.spigotmc.org>
2026-04-07 03:33:40 +0800

    fix: redirect to homepage immediately on 404 in Commerce theme

--- themes/commerce/index.js
+++ themes/commerce/index.js
@@ -301,31 +301,20 @@ const LayoutSlug = props => {
 const Layout404 = props => {
   const router = useRouter()
   useEffect(() => {
-    // 延时3秒如果加载失败就返回首页
-    setTimeout(() => {
-      if (isBrowser) {
-        const article = document.querySelector('#article-wrapper #notion-article')
-        if (!article) {
-          router.push('/').then(() => {
-            // console.log('找不到页面', router.asPath)
-          })
-        }
-      }
-    }, 3000)
-  })
+    // 立即跳转回首页
+    router.push('/')
+  }, [])
   return (
-    <>
-      <div className='text-black w-full h-screen text-center justify-center content-center items-center flex flex-col'>
-        <div className='dark:text-gray-200'>
-          <h2 className='inline-block border-r-2 border-gray-600 mr-2 px-3 py-2 align-top'>
-            404
-          </h2>
-          <div className='inline-block text-left h-32 leading-10 items-center'>
-            <h2 className='m-0 p-0'>页面未找到</h2>
-          </div>
+    <div className='text-black w-full h-screen text-center justify-center content-center items-center flex flex-col'>
+      <div className='dark:text-gray-200'>
+        <h2 className='inline-block border-r-2 border-gray-600 mr-2 px-3 py-2 align-top'>
+          404
+        </h2>
+        <div className='inline-block text-left h-32 leading-10 items-center'>
+          <h2 className='m-0 p-0'>页面未找到，正在跳转回首页...</h2>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
